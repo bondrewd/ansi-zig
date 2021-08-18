3f05c454f5396787956a88586fab66dd76660d3f
Diego Ugarte <diego.ugarte@outlook.com>
2021-08-19 08:27:01 +0900

    :art: Update build script and moved ansi.zig to main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,78 @@
-const std = @import("std");
-const testing = std.testing;
+// Format
+pub const reset = "\x1b[000m";
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub const bold_on = "\x1b[001m";
+pub const bold_off = "\x1b[021m";
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const dim_on = "\x1b[002m";
+pub const dim_off = "\x1b[022m";
+
+pub const italic_on = "\x1b[003m";
+pub const italic_off = "\x1b[023m";
+
+pub const underline_on = "\x1b[004m";
+pub const underline_off = "\x1b[024m";
+
+pub const slow_blink_on = "\x1b[005m";
+pub const rapid_blink_on = "\x1b[006m";
+pub const blink_off = "\x1b[025m";
+
+pub const inverse_on = "\x1b[007m";
+pub const inverse_off = "\x1b[027m";
+
+pub const hidden_on = "\x1b[008m";
+pub const hidden_off = "\x1b[028m";
+
+// Foreground color
+pub const fg_default = "\x1b[039";
+
+pub const fg_black = "\x1b[030m";
+pub const fg_white = "\x1b[097m";
+
+pub const fg_dark_red = "\x1b[031m";
+pub const fg_light_red = "\x1b[091m";
+
+pub const fg_dark_green = "\x1b[032m";
+pub const fg_light_green = "\x1b[092m";
+
+pub const fg_dark_yellow = "\x1b[033m";
+pub const fg_light_yellow = "\x1b[093m";
+
+pub const fg_dark_blue = "\x1b[034m";
+pub const fg_light_blue = "\x1b[094m";
+
+pub const fg_dark_magenta = "\x1b[035m";
+pub const fg_light_magenta = "\x1b[095m";
+
+pub const fg_dark_cyan = "\x1b[036m";
+pub const fg_light_cyan = "\x1b[096m";
+
+pub const fg_dark_gray = "\x1b[090m";
+pub const fg_light_gray = "\x1b[037m";
+
+// Background color
+pub const bg_default = "\x1b[049";
+
+pub const bg_black = "\x1b[040m";
+pub const bg_white = "\x1b[107m";
+
+pub const bg_dark_red = "\x1b[041m";
+pub const bg_light_red = "\x1b[101m";
+
+pub const bg_dark_green = "\x1b[042m";
+pub const bg_light_green = "\x1b[102m";
+
+pub const bg_dark_yellow = "\x1b[043m";
+pub const bg_light_yellow = "\x1b[103m";
+
+pub const bg_dark_blue = "\x1b[044m";
+pub const bg_light_blue = "\x1b[104m";
+
+pub const bg_dark_magenta = "\x1b[045m";
+pub const bg_light_magenta = "\x1b[105m";
+
+pub const bg_dark_cyan = "\x1b[046m";
+pub const bg_light_cyan = "\x1b[106m";
+
+pub const bg_dark_gray = "\x1b[100m";
+pub const bg_light_gray = "\x1b[047m";
